d58a9284319904314438c6d160a6c514354c8421
jcarroll <jcarroll@rallydev.com>
2014-09-03 08:30:07 -0500

    Initial Commit

---
+++ App.js
@@ -0,0 +1,81 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc3/doc/">App SDK 2.0rc3 Docs</a>'},
+    launch: function() {
+    		var deStore;
+
+    		var panel1 = Ext.create( 'Ext.panel.Panel', {
+    			layout : {
+    				type : 'hbox', // Arrange child items vertically
+    				align : 'middle', // Each takes up full width
+    			},
+    			width : '100%',
+    			bodyPadding : 10,
+    			bodyStyle : {
+    				background : '#9A9494',
+    			},
+    			items : [
+    					{
+    						xtype : 'container',
+    						itemId : 'iterationFilter'
+    					}, {
+    						xtype : 'rallybutton',
+    						text : 'Print Summary',
+    						handler : function () {
+    							this._printSummary();
+    						},
+    						style : {
+    							marginRight : '10px',
+    							marginLeft : '10px'
+    						},
+    						// flex:'1',
+    						scope : this
+    					}
+    			/*
+    			 * { xtype:'rallybutton', text: 'Print Defects', handler: function() { this._printDefects(); }, style: { marginLeft: '10px' },
+    			 * 
+    			 * flex:'1', scope:this }
+    			 */
+    			]
+    		} );
+    		this.add( panel1 );
+    		var panel2 = Ext.create( 'Ext.panel.Panel', {
+    			layout : 'vbox',
+    			width : '100%',
+    			bodyPadding : 10,
+    			bodyStyle : {
+    				background : '#9A9494',
+    			},
+    			items : [
+
+    				{
+    					xtype : 'container',
+    					layout : 'accordion',
+    					itemId : 'testStatus',
+    					width : '100%',
+    					height : 500
+    				}
+    			]
+    		} );
+
+    		this.add( panel2 );
+    		this._testSummary = [];
+    		this._testResultList = [];
+    		this._testSetTestList = [];
+    		var TestSetStore;
+    		var TestResultStore;
+    		scope: this;
+
+    		this.down( '#iterationFilter' ).add( {
+    			xtype : 'rallyiterationcombobox',
+    			itemId : 'iterationComboBox',
+    			listeners : {
+    				change : this._queryForTestSets,
+    				ready : this._queryForTestSets,
+    				scope : this
+    			}
+    		} );
+    		;
+    	},
+});
